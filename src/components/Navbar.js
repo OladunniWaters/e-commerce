904859f653e7075e8df773df7d57cb81bd02ebfd
Oladunni Waters <waters.oladunni@gmail.com>
2023-03-30 11:39:24 +0100

    adedd assets

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,8 +24,8 @@ function Navbar() {
        <nav class="navbar navbar-expand-lg">
         <div class="container">
           <a class="navbar-brand" href="/homepage">WATERS</a>
-          <button class="navbar-toggler-icon" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <RxHamburgerMenu />
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <RxHamburgerMenu className='navbar-toggler-icon'/>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
